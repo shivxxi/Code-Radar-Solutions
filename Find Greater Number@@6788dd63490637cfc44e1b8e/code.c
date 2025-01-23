ad5f8f66444e9b0482006ecb37f1135f2b6eb4e8
shivxxi <guptashivanii2006@gmail.com>
2025-01-23 12:38:48 +0530

    Solution to Find Greater Number

--- Find Greater Number@@6788dd63490637cfc44e1b8e/code.c
+++ Find Greater Number@@6788dd63490637cfc44e1b8e/code.c
@@ -3,9 +3,9 @@ int main(){
     int a,b;
     scanf("%d %d",&a,&b);
     if(a>b){
-        printf("%d",&a);
+        printf("%d",a);
     } else{
-        printf("%d",&b);
+        printf("%d",b);
     }
     return 0;
 }
